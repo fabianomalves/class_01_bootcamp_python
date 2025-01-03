3ab91ba125c3badb54883fac3345f843087fe393
fabianomalves <fabianomalves@proton.me>
2025-01-03 13:49:20 -0300

    Created the exercise file with the answers

---
+++ exercise_01.py
@@ -0,0 +1,28 @@
+BONUS_CONSTANT = 1000
+
+# 1) Ask the user to type his name
+
+name_user = input('Enter your name: ')
+
+# 2) Ask the user to type his salary value
+# Convert his salary to a float
+
+salary_value = float(input('Enter your salary value: '))
+
+
+# 3) Ask the user to type his bonus value
+# Convert his bonus to a float
+
+bonus_value = float(input('Enter your bonus value: '))    
+
+# 4) Calculate the final value bonus
+
+final_bonus = BONUS_CONSTANT + salary_value + bonus_value
+
+
+# 5) Print a message include the user name, salary and bonus
+
+print(f'The user {name_user}, have a final bonus value: {final_bonus}') 
+
+# 6) How many bugs can you verify at this program
+
