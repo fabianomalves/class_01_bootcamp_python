fe98e31e65eddc7ec8f87762f01b8dbd7bd50244
fabianomalves <fabianomalves@proton.me>
2025-01-03 13:48:40 -0300

    Created new file example_02.py

---
+++ example_02.py
@@ -0,0 +1,6 @@
+# Make a program that a user type 2 values and make the sum of this values
+
+value_1 = int(input('Type the first value: '))
+value_2 = int(input('Type the second value: '))
+
+print(value_1 + value_2)
